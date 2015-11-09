5c7e9d7cafadf1ce9f695c5373d291d813d28b8b
Paul Clifford <paulclifford2570@gmail.com>
2015-11-08 16:27:18 -0800

    Update SortFrame.java

--- SortFrame.java
+++ SortFrame.java
@@ -1,1 +1,20 @@
+import javax.swing.JFrame;
+
+public class SortFrame extends JFrame {
+	
+	public SortFrame(){
+		super("Two Sorts");
+		setVisible(true);
+		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		add(new SortPanel());
+		pack();
+	}
+	
+	public static void main(String[] args) {
+		
+		new SortFrame();
+
+	}
+
+}
 
