014a16beaddd17a7974a591a6614f61f65ad7c38
PCliffordSF <paulclifford2570@gmail.com>
2015-11-13 15:36:45 -0800

    cool stuff

--- SortPanel.java
+++ SortPanel.java
@@ -11,23 +11,25 @@ import javax.swing.JPanel;
 
 public class SortPanel extends JPanel {
 
-	JButton buildArrayButton;
-	JButton sortArrayButton;
-	int spacer;
-	int leftIndent;
-	int rInt;
-	int[] sortOneArray = new int[30];
-	int[] sortTwoArray = new int[30];
+	private JButton sortArrayButton;
+	private int spacer;
+	private int leftIndent;
+	private int rInt;
+	private Integer[] sortOneArray = new Integer[30];
+	private Integer[] sortTwoArray = new Integer[30];
+	private int index = 0;
+	private int changeIndexColor;
+	private int changeMinColor;
+	private boolean changeColor;
 
 	public SortPanel(){
 		setPreferredSize(new Dimension(800,500));
 		setLayout(new FlowLayout(FlowLayout.LEFT));
-		buildArrayButton = new JButton("Build New Arrays");
 		sortArrayButton = new JButton("Sort The Arrays");
-		buildArrayButton.addActionListener(new ButtonListener());
-		add(buildArrayButton);
+		sortArrayButton.addActionListener(new ButtonListener());
 		add(sortArrayButton);
 		buildArrays();
+		changeColor = false;
 	}
 
 	public void paintComponent(Graphics g){
@@ -40,15 +42,43 @@ public class SortPanel extends JPanel {
 		g.fillRect(0,0,getWidth(), getHeight());
 		g.setColor(Color.GREEN);
 
-		for(int i = 0; i < 30; i++){
+       	for(int i = 0; i < 30; i++){
+       		if((i == changeIndexColor | i == changeMinColor) & changeColor == true){
+       			g.setColor(Color.WHITE);
+       		}
 			g.fillRect(leftIndent + spacer + 20*i ,190 - sortOneArray[i], 10, sortOneArray[i]);
+			g.setColor(Color.GREEN);
 		}
-		    g.setColor(Color.MAGENTA);
+
+		g.setColor(Color.MAGENTA);
 		for(int j = 0; j < 30; j++){
 			g.fillRect(leftIndent + spacer + 20*j ,200 , 10, sortTwoArray[j]);
 		}
+}
+	public void selectionSort (Comparable[] sortOneArray){
+        int min;
+        Integer tempArray;
+        min = index;
 
+        for (int scan = index + 1; scan < sortOneArray.length; scan++){
+            if (sortOneArray[scan].compareTo(sortOneArray[min]) < 0){
+            min = scan;
+            }
+
+        }
+            // Swap the values
+            tempArray = (Integer) sortOneArray[min];
+            sortOneArray[min] = sortOneArray[index];
+            sortOneArray[index] = tempArray;
+            index = index + 1;
+            changeIndexColor = index;
+            changeMinColor = min;
+            changeColor = true;
+            if(min == index -1){
+            	changeColor = false;
+            }
 	}
+
 	public void buildArrays(){
 		for(int x = 0; x < 30; x++){
 			rInt = (int)(Math.random()*7) + 1;
@@ -56,11 +86,31 @@ public class SortPanel extends JPanel {
 			sortTwoArray[x] = rInt*10;
 		}
 	}
-
 	   private class ButtonListener implements ActionListener{
 	      public void actionPerformed (ActionEvent event){
-	    	buildArrays();
+	    	selectionSort(sortOneArray);
 	    	repaint();
 	      }
 	   }
 }
+
+//////////////// INSERTION SORT ALGORITHM ////////////////////////////////////////
+/*
+public static void insertionSort (Comparable[] list)
+{
+   for (int index = 1; index < list.length; index++)
+   {
+      Comparable key = list[index];
+      int position = index;
+
+      //  Shift larger values to the right
+      while (position > 0 && key.compareTo(list[position-1]) < 0)
+      {
+         list[position] = list[position-1];
+         position--;
+      }
+
+      list[position] = key;
+   }
+}
+*/
