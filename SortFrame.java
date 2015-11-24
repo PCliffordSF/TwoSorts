2ad49c3c9ef0813b1a3afc9bee0830ac16310446
PCliffordSF <paulclifford2570@gmail.com>
2015-11-23 16:49:54 -0800

    Correct File Name

--- SortFrame.java
+++ SortFrame.java
@@ -1,19 +1,30 @@
+//********************************************************************
+//  SortFrame.java       Authors: Paul Clifford/ Patrick Kelly / Amy Dos Santos
+//
+//  Constructs the frame and panel for Two Sorts
+//  Assigment number 7, Due November 24th, CS 111B, 1:00pm section
+//********************************************************************
+
+// Algorithm
+// 1. Instantiate new Frame and new Panel
+
+
 import javax.swing.JFrame;
 
 public class SortFrame extends JFrame {
 
-	SortPanel sortPanel;
+	TwoSortsPanel twoSortsPanel;
 
-	public SortFrame(SortPanel sortPanel){
+	public SortFrame(TwoSortsPanel twoSortsPanel){
 		super("Two Sorts");
-		this.sortPanel = sortPanel;
+		this.twoSortsPanel = twoSortsPanel;
 		setVisible(true);
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		add(sortPanel);
+		add(twoSortsPanel);
 		pack();
 	}
 
 	public static void main(String[] args) {
-		new SortFrame(new SortPanel());
+		new SortFrame(new TwoSortsPanel());
 	}
 }
